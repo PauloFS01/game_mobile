103edabb2e214604ccf5af42c3bcbb940f5f4fd8
Paulo <paulofs10.sea@gmail.com>
2022-09-01 22:23:49 -0300

    feature destry rings

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private int score;
+    public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,15 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void GameScore(int ringScore)
+    {
+        score += ringScore;
+        scoreText.text = score.ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- My project/Assets/Scripts/Ring.cs
+++ My project/Assets/Scripts/Ring.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Ring : MonoBehaviour
 {
+    public Transform Ball;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class Ring : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(transform.position.y +17 >= Ball.transform.position.y)
+        {
+            Destroy(gameObject);
+        }
         
     }
 }
